8aeb74e8ef804e1c782deb58b71dbac81008e231
perrywco <perrywco@13b049d0-06c1-4c5f-8868-ce216834a2d3>
2009-01-03 10:47:02 +0000

    cleanup + retry session key settings from config.yml
    git-svn-id: svn+ssh://hashmapsarenotfree.com/home/perrywco/svn_perryw/repo/restful_authentication_tutorial@204 13b049d0-06c1-4c5f-8868-ce216834a2d3

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
-ENV['RAILS_ENV'] ||= 'development'
+ENV['RAILS_ENV'] ||= 'production'
 
 require 'yaml'
 require 'rubygems'
