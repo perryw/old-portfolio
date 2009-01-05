a09e4d476f910089d50148e2a20412b83d5492d5
perrywco <perrywco@13b049d0-06c1-4c5f-8868-ce216834a2d3>
2009-01-05 02:50:57 +0000

    scriptculous's inplace editor won't work without the 3rd parameter, even if it is empty, so edited in_place_macros_helper.rb to add the 3rd parameter no matter what
    git-svn-id: svn+ssh://hashmapsarenotfree.com/home/perrywco/svn_perryw/repo/restful_authentication_tutorial@211 13b049d0-06c1-4c5f-8868-ce216834a2d3

--- vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
+++ vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
@@ -39,28 +39,27 @@ module InPlaceMacrosHelper
     function << "'#{field_id}', "
     function << "'#{url_for(options[:url])}'"
 
-    js_options = {}
-
     if protect_against_forgery?
       options[:with] ||= "Form.serialize(form)"
       options[:with] += " + '&authenticity_token=' + encodeURIComponent('#{form_authenticity_token}')"
     end
-    
-    js_options['cancelText'] = %('#{options[:cancel_text]}') if options[:cancel_text]
-    js_options['okText'] = %('#{options[:save_text]}') if options[:save_text]
-    js_options['loadingText'] = %('#{options[:loading_text]}') if options[:loading_text]
-    js_options['savingText'] = %('#{options[:saving_text]}') if options[:saving_text]
-    js_options['rows'] = options[:rows] if options[:rows]
-    js_options['cols'] = options[:cols] if options[:cols]
-    js_options['size'] = options[:size] if options[:size]
-    js_options['externalControl'] = "'#{options[:external_control]}'" if options[:external_control]
-    js_options['loadTextURL'] = "'#{url_for(options[:load_text_url])}'" if options[:load_text_url]        
-    js_options['ajaxOptions'] = options[:options] if options[:options]
-    js_options['htmlResponse'] = !options[:script] if options[:script]
-    js_options['callback']   = "function(form) { return #{options[:with]} }" if options[:with]
-    js_options['clickToEditText'] = %('#{options[:click_to_edit_text]}') if options[:click_to_edit_text]
-    js_options['textBetweenControls'] = %('#{options[:text_between_controls]}') if options[:text_between_controls]
-    function << (', ' + options_for_javascript(js_options)) unless js_options.empty?
+    js_options = {}
+      js_options['cancelText'] = %('#{options[:cancel_text]}') if options[:cancel_text]
+      js_options['okText'] = %('#{options[:save_text]}') if options[:save_text]
+      js_options['loadingText'] = %('#{options[:loading_text]}') if options[:loading_text]
+      js_options['savingText'] = %('#{options[:saving_text]}') if options[:saving_text]
+      js_options['rows'] = options[:rows] if options[:rows]
+      js_options['cols'] = options[:cols] if options[:cols]
+      js_options['size'] = options[:size] if options[:size]
+      js_options['externalControl'] = "'#{options[:external_control]}'" if options[:external_control]
+      js_options['loadTextURL'] = "'#{url_for(options[:load_text_url])}'" if options[:load_text_url]        
+      js_options['ajaxOptions'] = options[:options] if options[:options]
+      js_options['htmlResponse'] = !options[:script] if options[:script]
+      js_options['callback']   = "function(form) { return #{options[:with]} }" if options[:with]
+      js_options['clickToEditText'] = %('#{options[:click_to_edit_text]}') if options[:click_to_edit_text]
+      js_options['textBetweenControls'] = %('#{options[:text_between_controls]}') if options[:text_between_controls]
+      #function << (', ' + options_for_javascript(js_options)) unless js_options.empty?
+      function << (', ' + options_for_javascript(js_options))
     
     function << ')'
 
