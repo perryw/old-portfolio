2ee9ebf3e1c40d70d5b06f787fcffa25e99f8c36
perrywco <perrywco@13b049d0-06c1-4c5f-8868-ce216834a2d3>
2009-01-19 10:32:50 +0000

    removed migration since it wasn't needed
    git-svn-id: svn+ssh://hashmapsarenotfree.com/home/perrywco/svn_perryw/repo/restful_authentication_tutorial@285 13b049d0-06c1-4c5f-8868-ce216834a2d3

--- db/migrate/20090119035510_add_has_attachment_to_resources.rb
+++
@@ -1,9 +0,0 @@
-class AddHasAttachmentToResources < ActiveRecord::Migration
-  def self.up
-    #add_column :resources, "has_image", :boolean
-    Resource.reset_column_information
-  end
-
-  def self.down
-  end
-end
