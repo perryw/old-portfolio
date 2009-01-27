997ff0701c4194724d130025b66e719e618e3b75
perrywco <perrywco@13b049d0-06c1-4c5f-8868-ce216834a2d3>
2009-01-27 00:30:03 +0000

    multiple selection of tags in courses/list
    git-svn-id: svn+ssh://hashmapsarenotfree.com/home/perrywco/svn_perryw/repo/restful_authentication_tutorial@329 13b049d0-06c1-4c5f-8868-ce216834a2d3

--- public/javascripts/wyrrep_utility.js
+++ public/javascripts/wyrrep_utility.js
@@ -161,7 +161,7 @@ moveTo = function(container_prefix, container_id, tagname){
 	$('more_info_'+container_id).innerHTML='less info';
 	
 	$$('#'+container_prefix+container_id + ' .deliv_show').each( function(ds) { /* hide deliverable div if it doesn't contain the tag */
-		if( ds.select('.'+tagname).size() == 0 )	{
+		if( ds.select('.'+tagname).size() == 0 && ds.select('.highlight').size() == 0 )	{
 			new Effect.toggle(ds.ancestors()[1], 'appear');
 		}
 	});
