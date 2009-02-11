3dade3553de6fbe13c9af37f3aa8a113843407cb
perrywco <perrywco@13b049d0-06c1-4c5f-8868-ce216834a2d3>
2009-02-11 05:42:33 +0000

    test moving loading div into body and keeping scripts in the header.  might result in slow page loads
    git-svn-id: svn+ssh://hashmapsarenotfree.com/home/perrywco/svn_perryw/repo/restful_authentication_tutorial@375 13b049d0-06c1-4c5f-8868-ce216834a2d3

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Event.observe(window, 'dom:loaded', function(){
+  this.interval = setInterval(function(){
+    if(LOADING_STAGE == '' ) clearInterval(this.interval);
+    $('loading').update('Loading ' + LOADING_STAGE + '...');
+  }, 50);
+});
